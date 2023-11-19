28ddbc2d4008f0ed04e10e3d7fe99edeefc80e1f
Meils.zjj <1392372716@qq.com>
2023-11-19 21:55:05 +0800

    Update PageLayout.tsx

--- src/components/page-layout/PageLayout.tsx
+++ src/components/page-layout/PageLayout.tsx
@@ -1,41 +1,50 @@
-import React from 'react';
-import { Layout } from '@arco-design/web-react';
+import React, { memo } from 'react';
+import { Layout, Spin } from '@arco-design/web-react';
 
 import { PageLayoutContainerPropsType } from './props-type';
+import { useSelector } from '@/store/hooks';
 
 // components
 import Sidebar from '@/components/sidebar/Sidebar';
 import NavBar from '@/components/nav-bar/NavBar';
 import MenuList from '@/components/menu-list/MenuList';
+import KeepAliveTabContent from '@/components/keep-alive-tab-content/KeepAliveContent';
 
 import styles from './styles/style.module.less';
 
 /** @component 页面布局组件 */
 const PageLayoutContainer: React.FC<PageLayoutContainerPropsType> = () => {
+  const { hasLoaded: menuLoaded } = useSelector((state) => state.menu);
+  const { hasLoaded: userLoaded } = useSelector((state) => state.user);
+
+  const hasLoaded = menuLoaded && userLoaded;
+
+  if (!hasLoaded) {
+    return <Spin size={40} className={styles['spin']} />;
+  }
+
   return (
-    <Layout className={styles['page-layout-container']}>
+    <div className={styles['page-layout-container']}>
       <div className={styles['page-layout__slide']}>
-        {/* 侧边栏 - 一级菜单 */}
+        {/* 侧边栏「一级菜单」 */}
         <Sidebar />
       </div>
-
       <div className={styles['page-layout__main']}>
         <div className={styles['page-layout__navbar']}>
           {/* 顶部导航条 */}
           <NavBar />
         </div>
-        <Layout>
-          <MenuList></MenuList>
-          <Layout className={styles['layout-content']}>
-            <div className={styles['layout-content-wrapper']}>
-              {/* 内容 */}
-              <p>内容</p>
-            </div>
-          </Layout>
+        <Layout className={styles['page-layout__bottom']}>
+          {/* 二级菜单列表 */}
+          <MenuList />
+          <div className={styles['layout-content-wrapper']}>
+            {/* 核心内容展示区域 */}
+            <KeepAliveTabContent />
+          </div>
         </Layout>
       </div>
-    </Layout>
+    </div>
   );
 };
 
-export default PageLayoutContainer;
+export default memo(PageLayoutContainer);
